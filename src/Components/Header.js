8f312432f21b89f0d9ee2e2ca2ba332673f2ce84
Daniela Correa <dacomu24@gmail.com>
2022-04-26 21:46:38 -0500

    Working in a new react project, this time a meme generator

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className = 'header'>
+            <img src="Images/trollFace.png" alt = "Troll face"></img>
+            <h1>Meme generator</h1>
+            <h4>React course - Project 3 </h4>
+
+        </div>
+    )
+}
